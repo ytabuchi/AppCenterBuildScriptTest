3e13bf7bceef0aa28776de258f1da5dc278d3ea1
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2018-10-15 22:29:36 +0900

    add-aractivity

--- BuildTest/app/src/main/java/eu/kudan/ar/MainActivity.kt
+++ BuildTest/app/src/main/java/eu/kudan/ar/MainActivity.kt
@@ -1,12 +1,101 @@
 package eu.kudan.ar
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import android.app.AlertDialog
+import android.content.DialogInterface
+import eu.kudan.kudan.*
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.support.v4.view.accessibility.AccessibilityEventCompat.setAction
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ARActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val key = ARAPIKey.getInstance()
+        key.setAPIKey("agWZcpYLYjBxCbWf2qZx6k+PWISqeGtFCqKaZwYtwS+kdn1HKiQAmsJ55STRBe9BqCw3VwG6qL+ESI5ntTF/iV/uekLG3PCokaUE0/uTzqhaYlxRdmuNBIduzBCjq3mV2na+gy3ffHH9Ipc7eIN0geTj3p+ppsmK0U399iGmN38ndIh6k2y16cByWIecMSU3yw3Ztw7gHRqf83hVhZ5T2ACGK4SNkQhhdKp+CTaR5W3amYCJBgwumqFqNFyI9UniuMk70T/cQObRQum2U51OjjbMfmEAwIBt8Q8jD2yACzye6K4/1O4pZhbGEbiDeLrAfxqMwBAe5o6vnYIilGNnpDhfi3wOHhRaqtLOVvB58GUIFTnAPvmYFVnLWRJmCUZ9FJNDyX3ALCl/alFEWh+A/a6NFjcwLGKI9drPuGG4ONFg4p0l+p3b9DZoLzszlmWAflI/UFzQa++kQn3/sclO9i0vPnpi0LWoABm5vGswLVAIX/0k6384GXxfkADI6fjGtf62XJ5ImaVDiiREa9mabWEQGoifghQG1sGNDYgBIYEpiaLsVzOfTALpe20Q7kFCMjedJImQhhuLtEK1BXfXJEed1QqUOsG9IeKxKk28GbOtOF9w3yrSF3gnJslzZxF2kEF3C6ckog8byagS+4p37FJmbpPsiKNH1Qm0LuouGcQ=")
+
+        permissionsRequest()
+    }
+
+    override fun setup() {
+
+        // Initialise the image trackable and load the image.
+        val imageTrackable = ARImageTrackable("lego")
+        imageTrackable.loadFromAsset("lego.jpg") ?: return
+
+        // Get the single instance of the image tracker.
+        val trackableManager = ARImageTracker.getInstance()
+        trackableManager.initialise()
+
+        //Add the image trackable to the image tracker.
+        trackableManager.addTrackable(imageTrackable)
+
+        // Initialise the image node with our image
+        val imageNode = ARImageNode("cow.png")
+
+        // Add the image node as a child of the trackable's world
+        imageTrackable.world.addChild(imageNode)
+
+        // imageNode のサイズを Trackable のサイズに合わせる
+        val textureMaterial = imageNode.material as ARTextureMaterial
+        val scale = imageTrackable.width / textureMaterial.texture.width
+        imageNode.scaleByUniform(scale)
+    }
+
+    // Permission のリクエストを OS 標準の requestPermissions メソッドで行う
+    private fun permissionsRequest(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA), 111)
+
+        }
+    }
+
+    // ダイアログを表示して、本サンプルアプリの設定画面に遷移する
+    private fun permissionsNotSelected() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Permissions required")
+        builder.setMessage("Please enable the requested permissions in the app settings in order to use this demo app")
+        builder.setPositiveButton("Set permission", DialogInterface.OnClickListener { dialog, id ->
+            dialog.cancel()
+            val intent = Intent()
+            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            intent.data = Uri.parse("package:eu.kudan.ar")
+            startActivity(intent)
+        })
+        val noPermission = builder.create()
+        noPermission.show()
+    }
+
+    // requestPermissions　ダイアログの結果を受け、全て許可されていなければ、permissionsNotSelected を呼び出し
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            111 -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[2] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[3] == PackageManager.PERMISSION_GRANTED) {
+                    // パーミッションが必要な処理
+                } else {
+                    permissionsNotSelected()
+                }
+            }
+        }
     }
 }
