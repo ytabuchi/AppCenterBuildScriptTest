a6e6035ab2151a79bedc7b89e11e426df0cf011a
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2018-10-15 23:09:28 +0900

    re-try script

--- BuildTest/app/appcenter-post-clone.sh
+++ BuildTest/app/appcenter-post-clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Example: Get required frameworks.
-curl -O https://github.com/ytabuchi/KudanAR-assets/blob/master/android/KudanAR.aar
+curl -OL https://github.com/ytabuchi/KudanAR-assets/raw/master/android/KudanAR.aar
 mv -f ./KudanAR.aar ../KudanAR/KudanAR.aar
 ls ../KudanAR/
